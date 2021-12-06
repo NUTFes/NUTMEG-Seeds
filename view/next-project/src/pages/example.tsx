4636f8e1f329c7a7a774f09fc0ce0ae8f2a56909
YushiroDodo63 <yushiro0603@gmail.com>
2021-12-06 21:16:36 +0900

    [fix]GlassCardの作成(#3)

--- view/next-project/src/pages/example.tsx
+++ view/next-project/src/pages/example.tsx
@@ -1,17 +1,19 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styled from 'styled-components'
-import MainLayout from '@components/MainLayout'
-import FlatCard from '@components/FlatCard'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styled from "styled-components";
+import MainLayout from "@components/layout/MainLayout";
+import FlatCard from "@components/common/FlatCard";
+import GlassCard from "@components/common/GlassCard";
 
 export default function Example() {
+  const imagePath = "/header-logo.svg";
   return (
     <MainLayout>
-      <FlatCard>Hello World</FlatCard>
-      <FlatCard>Hello World</FlatCard>
-      <FlatCard>Hello World</FlatCard>
+      <GlassCard>
+        <Image src={imagePath} height={30} width={142} />
+      </GlassCard>
       <FlatCard>Hello World</FlatCard>
     </MainLayout>
-  )
+  );
 }
