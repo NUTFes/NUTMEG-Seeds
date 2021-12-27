cb9294473688309c259c016d5d31718630595987
Kugue99A <hisabisa99yohey@gmail.com>
2021-12-27 16:46:35 +0900

    [mod] GETメソッドの実装 (#12)

--- view/next-project/src/utils/api_methods.ts
+++ view/next-project/src/utils/api_methods.ts
@@ -1,12 +1,28 @@
 import fetch from 'node-fetch'
 
-type Url = {
-  url?: URL 
-}
-
-const get = async (url: Url) => {
+export const get = async (url: string) => {
   await fetch(url, {
+    method: 'GET',
+    headers: {'Content-Type': 'application/json'}
   })
 };
 
-export default get;
+export const get_with_token = async (url: string) => {
+  await fetch(url, {
+    method: 'GET',
+    headers: {
+      "Content-Type": 'application/json',
+      "access-token": localStorage.getItem("access-token") || "none",
+    }
+  })
+};
+
+export const post = async (url: string) => {
+  await fetch(url, {
+    method: 'POST',
+    headers: {
+      "Content-Type": 'application/json',
+    }
+  })
+};
+
