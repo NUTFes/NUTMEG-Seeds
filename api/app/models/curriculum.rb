7dc3b0f6b1d4d0c98084cae87e18ad6cc76f06f5
Kugue99A <hisabisa99yohey@gmail.com>
2022-02-23 14:49:10 +0900

    [add] カリキュラム個別ページ用のAPI (#36)

--- api/app/models/curriculum.rb
+++ api/app/models/curriculum.rb
@@ -1,4 +1,28 @@
 class Curriculum < ApplicationRecord
   belongs_to :skill
   has_many :records
+
+  def self.with_skills
+    @records = Curriculum.preload(:skill)
+      .map{
+        |curriculum|
+        {
+          "curriculum": curriculum,
+          "skill": curriculum.skill.name
+        }
+      }
+  end
+
+  def self.with_skills_and_records(curriculum_id)
+    @record = Curriculum.eager_load(:skill, :records).where(curriculums: {id: curriculum_id})
+      .map{
+        |curriculum|
+        {
+          "curriculum": curriculum,
+          "skill": curriculum.skill.name,
+          "records": curriculum.records
+        }
+      }
+  end
+
 end
