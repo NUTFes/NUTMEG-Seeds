46bc0dc6fae48abb95df1544125a4200c9668ca5
tsubasaoura <tsubasaoura@OURAS-Laptop.localdomain>
2022-07-18 11:29:08 +0900

    [mod] デプロイのための設定

--- view/next-project/next.config.js
+++ view/next-project/next.config.js
@@ -7,9 +7,7 @@ module.exports = {
     optimizeFonts: true,
   },
   env: {
-    //SSR_API_URI: isProd ? 'https://seeds-api.nutfes.net' : 'http://seeds_api:3000',
-    //CSR_API_URI: isProd ? 'https://seeds-api.nutfes.net' : 'http://localhost:3000',
-    SSR_API_URI: isProd ? 'http://seeds_api:3000' : 'http://seeds_api:3000',
-    CSR_API_URI: isProd ? 'http://localhost:3000' : 'http://localhost:3000',
+    SSR_API_URI: isProd ? 'https://seeds-api.nutfes.net' : 'http://seeds_api:3000',
+    CSR_API_URI: isProd ? 'https://seeds-api.nutfes.net' : 'http://localhost:3000',
   },
 };
