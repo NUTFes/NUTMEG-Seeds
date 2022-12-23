52350f12fed8ab57bf28e377394597d79b523f3a
YushiroDodo63 <yushiro0603@gmail.com>
2022-12-24 01:54:21 +0900

    [fix] remove skill_ids

--- api/db/fixtures/curriculum.rb
+++ api/db/fixtures/curriculum.rb
@@ -1,6 +1,6 @@
 Curriculum.seed( :id,
-  { id: 1, title: 'TypeScript勉強会', graduation_assignment: 'チャットアプリを作成', skill_ids: 1},
-  { id: 2, title: 'Go勉強会', graduation_assignment: '家計簿アプリを作成', skill_ids: 1},
-  { id: 3, title: 'React勉強会', graduation_assignment: 'Webアプリを作成', skill_ids: 2 },
-  { id: 4, title: 'インターフェース勉強会', graduation_assignment: 'ポートフォリオを作成', skill_ids: 3 },
+  { id: 1, title: 'TypeScript勉強会', graduation_assignment: 'チャットアプリを作成'},
+  { id: 2, title: 'Go勉強会', graduation_assignment: '家計簿アプリを作成'},
+  { id: 3, title: 'React勉強会', graduation_assignment: 'Webアプリを作成' },
+  { id: 4, title: 'インターフェース勉強会', graduation_assignment: 'ポートフォリオを作成' },
 )
