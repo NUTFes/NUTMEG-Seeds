179fb90c5d639095665e532c7cacf07ac174692b
imaimai17468 <tosiki_801_life@outlook.jp>
2023-01-25 01:27:59 +0900

    [feat]花火アニメーションの追加

--- view/next-project/src/components/common/RecordAddAnimation/RecordAddAnimation.tsx
+++ view/next-project/src/components/common/RecordAddAnimation/RecordAddAnimation.tsx
@@ -1,12 +1,26 @@
 import AnimationButton from '../AnimationButton';
 import s from './RecordAddAnimation.module.css';
-import { useCallback } from 'react';
-import Particles from 'react-tsparticles';
-import { loadFull } from 'tsparticles';
+import Particles from 'react-particles';
+import type { Engine } from 'tsparticles-engine';
+import { loadFireworksPreset } from 'tsparticles-preset-fireworks';
 
 const RecordAddAnimation = () => {
+  const onInit = async (main: Engine): Promise<void> => {
+    await loadFireworksPreset(main);
+  };
+
+  const options = {
+    preset: 'fireworks',
+    fullScreen: { enable: false },
+    background: {
+      color: { value: '#f5f5f5' },
+      opacity: 0,
+    },
+  };
+
   return (
     <div className={s.container}>
+      <Particles options={options} init={onInit} className={s.particles} />
       <h1 className={s.title}>Congratulation</h1>
       <div className={s.buttons}>
         <AnimationButton>追加したレコードを見る</AnimationButton>
