71d406b4ca517f0f8833b377707a9d60e428c9d9
imaimai17468 <tosiki_801_life@outlook.jp>
2023-01-29 20:09:25 +0900

    [feat]add HeaderMenu

---
+++ view/next-project/src/components/common/HeaderMenu/HeaderMenu.tsx
@@ -0,0 +1,37 @@
+import React, { FC, useState } from 'react';
+import router from 'next/router';
+import s from './HeaderMenu.module.css';
+
+const HeaderMenu: FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const logout = () => {
+    localStorage.clear();
+    router.push('/');
+  };
+
+  return (
+    <div className={s.container}>
+      <button className={s.button} onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        Menu
+      </button>
+      {isMenuOpen && (
+        <div className={s.menu}>
+          <div
+            className={s.menuItem}
+            onClick={() => {
+              router.push('/profile');
+            }}
+          >
+            Profile
+          </div>
+          <div className={s.menuItem} onClick={logout}>
+            Logout
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HeaderMenu;
