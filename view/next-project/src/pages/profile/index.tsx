7c357ddf5941d07fa0de9b0980c22ed418ebe235
imaimai17468 <tosiki_801_life@outlook.jp>
2023-02-14 22:54:57 +0900

    [feat]プロフィール変更のフォームを追加

--- view/next-project/src/pages/profile/index.tsx
+++ view/next-project/src/pages/profile/index.tsx
@@ -5,11 +5,6 @@ import MainLayout from '@components/layout/MainLayout';
 import FlatCard from '@components/common/FlatCard';
 import s from './index.module.css';
 
-interface Props {
-  user: User[];
-  detail: UserDetail[];
-}
-
 interface User {
   id: number;
   name: string;
@@ -17,9 +12,10 @@ interface User {
 }
 
 interface UserDetail {
-  grade: number;
-  department: number;
-  bureau: number;
+  user_id: number;
+  grade_id: number;
+  department_id: number;
+  bureau_id: number;
   icon_name: string;
   github: string;
   slack: string;
@@ -31,27 +27,60 @@ interface UserDetail {
   pc_storage: string;
 }
 
-export async function getServerSideProps({ params }: any) {
-  console.log(params);
-  const getUserUrl = process.env.SSR_API_URI + '/api/v1/users';
-  const getUserRes = await get(getUserUrl);
-  const getDetailUrl = process.env.SSR_API_URI + '/api/v1/user_details';
-  const getDetailRes = await get(getDetailUrl);
+interface Grade {
+  id: number;
+  name: string;
+}
+
+interface Department {
+  id: number;
+  name: string;
+}
+
+interface Bureau {
+  id: number;
+  name: string;
+}
+
+interface Props {
+  gradeList: Grade[];
+  departmentList: Department[];
+  bureauList: Bureau[];
+}
+
+export const getServerSideProps = async () => {
+  const gradeListUrl = process.env.SSR_API_URI + '/grades';
+  const gradeList = await get(gradeListUrl);
+
+  const departmentListUrl = process.env.SSR_API_URI + '/departments';
+  const departmentList = await get(departmentListUrl);
+
+  const bureauListUrl = process.env.SSR_API_URI + '/bureaus';
+  const bureauList = await get(bureauListUrl);
+
   return {
     props: {
-      user: getUserRes,
-      detail: getDetailRes
+      gradeList,
+      departmentList,
+      bureauList,
     },
   };
 }
 
 export default function Profile(props: Props) {
-  const user = props.user;
-  const detail = props.detail;
+  const [userDetail, setUserDetail] = useState<UserDetail>();
 
   useEffect(() => {
-    console.log(props);
-  }, [props]);
+    const fetchUserDetail = async () => {
+      const userDetailUrl = process.env.CSR_API_URI + '/user_details/' + 1;
+      setUserDetail(await get(userDetailUrl));
+    };
+    fetchUserDetail();
+  }, []);
+
+  useEffect(() => {
+    console.log(userDetail);
+  }, [userDetail]);
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -68,12 +97,59 @@ export default function Profile(props: Props) {
               <p>Email</p>
               <input type='text' placeholder={email} className={s.input} />
               <button className={s.button}>Update</button>
+            </div>
+            <div className={s.setting}>
               <p>Password</p>
               <input type='text' placeholder={password} className={s.input} />
               <button className={s.button}>Update</button>
             </div>
+          </div>
+          <div>
             <h2 className={s.sub}>Edit Profile</h2>
             <hr />
+            <div className={s.setting}>
+              <p>学年</p>
+              <select className={s.select}>
+                {props.gradeList.map((grade) => (
+                  <option key={grade.id} value={grade.id}>
+                    {grade.name}
+                  </option>
+                ))}
+              </select>
+              <button className={s.button}>Update</button>
+            </div>
+            <div className={s.setting}>
+              <p>所属</p>
+              <select className={s.select}>
+                {props.departmentList.map((department) => (
+                  <option key={department.id} value={department.id}>
+                    {department.name}
+                  </option>
+                ))}
+              </select>
+              <button className={s.button}>Update</button>
+            </div>
+            <div className={s.setting}>
+              <p>局</p>
+              <select className={s.select}>
+                {props.bureauList.map((bureau) => (
+                  <option key={bureau.id} value={bureau.id}>
+                    {bureau.name}
+                  </option>
+                ))}
+              </select>
+              <button className={s.button}>Update</button>
+            </div>
+            <div className={s.setting}>
+              <p>GitHub</p>
+              <input type='text' placeholder={userDetail?.github} className={s.input} />
+              <button className={s.button}>Update</button>
+            </div>
+            <div className={s.setting}>
+              <p>紹介文</p>
+              <textarea placeholder={userDetail?.biography} className={s.textarea} />
+              <button className={s.button}>Update</button>
+            </div>
           </div>
         </FlatCard>
       </MainLayout>
