7ab95f5484b88d6783c97d4aeb4237a9d402241f
YushiroDodo63 <yushiro0603@gmail.com>
2023-02-14 21:11:41 +0900

    [add] create chapter API

---
+++ api/app/controllers/api/v1/chapters_api_controller.rb
@@ -0,0 +1,13 @@
+class Api::V1::ChaptersApiController < ApplicationController
+
+  def get_chapters_for_index
+    @chapters = Chapter.with_records
+    render json: @chapters
+  end
+
+  def get_chapter_for_detail
+    @chapters = Chapter.for_chapter_detail(params[:id])
+    render json: @chapters[0]
+  end
+
+end
