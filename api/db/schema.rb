41e9217d57b672cccb24d4c788ed48edd0c55cdb
noaikarashi <s211004@stn.nagaokaut.ac.jp>
2023-02-14 19:27:15 +0900

    [mod] カラム追加によるschemaの変更

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_28_101332) do
+ActiveRecord::Schema.define(version: 2023_02_07_094508) do
 
   create_table "bureaus", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -139,6 +139,7 @@ ActiveRecord::Schema.define(version: 2022_12_28_101332) do
     t.string "pc_storage"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "type_id"
   end
 
   create_table "user_skills", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -165,7 +166,6 @@ ActiveRecord::Schema.define(version: 2022_12_28_101332) do
     t.text "tokens"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "type_id"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
