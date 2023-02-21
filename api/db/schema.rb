130f365f9d79f3a59af12fe252d63aa6ab1346c4
YushiroDodo63 <yushiro0603@gmail.com>
2023-02-21 18:42:54 +0900

    [fix] add order column to chapter

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_14_112104) do
+ActiveRecord::Schema.define(version: 2023_02_21_091509) do
 
   create_table "bureaus", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2023_02_14_112104) do
     t.integer "curriculum_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "order"
   end
 
   create_table "curriculum_skills", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
