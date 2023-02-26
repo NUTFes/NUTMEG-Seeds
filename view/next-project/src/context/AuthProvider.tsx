63e5a955befac4718c51b35744b6ab3fc4722bff
imaimai17468 <tosiki_801_life@outlook.jp>
2023-02-27 03:50:28 +0900

    [feat] auth providerの実装

---
+++ view/next-project/src/context/AuthProvider.tsx
@@ -0,0 +1,53 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { userState } from 'src/store/user';
+
+interface User {
+  accessToken: string
+  client: string
+  uid: string
+  tokenType: string
+  userId: string
+}
+
+interface Props {
+  children: React.ReactNode;
+}
+
+interface AuthContext {
+  isAuthenticated: boolean;
+  currentUser: User | undefined;
+}
+
+const AuthContext = React.createContext<AuthContext>({
+  isAuthenticated: false,
+  currentUser: undefined
+});
+
+const AuthProvider = (props: Props) => {
+  const userParams = useRecoilValue(userState);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [currentUser, setCurrentUser] = useState<User | undefined>(undefined);
+
+  useEffect(() => {
+    if (userParams.userId !== '') {
+      setIsAuthenticated(true);
+      setCurrentUser(userParams);
+    }else{
+      setIsAuthenticated(false);
+      setCurrentUser(undefined);
+    }
+  }, [userParams]);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, currentUser }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+}
+
+export default AuthProvider;
