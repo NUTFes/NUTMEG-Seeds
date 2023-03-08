b55ea793c47828c6c753ae1e0f4d1209e826431d
imaimai17468 <tosiki_801_life@outlook.jp>
2023-03-08 21:49:13 +0900

    [fix] Tableコンポーネントにreact-data-tableを導入

--- view/next-project/src/components/common/Table/Table.tsx
+++ view/next-project/src/components/common/Table/Table.tsx
@@ -1,23 +1,67 @@
 import React, { FC } from 'react';
 import s from './Table.module.css';
+import DataTable from 'react-data-table-component';
+import { useRouter } from 'next/router';
+import { ReactNode } from 'react';
 
-interface TableContentProps {
-  headers: string[];
-  children: any;
+interface Column {
+  name: ReactNode;
+  selector: (row: any) => string;
+  sortable: boolean;
+  style?: any;
 }
 
+interface TableContentProps {
+  columns: Column[];
+  data: any;
+  route: 'records' | 'categories' | 'skills' | 'curriculums';
+}
+
+const CUSTOM_STYLES = {
+  headCells: {
+    style: {
+      fontSize: '14px',
+      justifyContent: 'center',
+    },
+  },
+  cells: {
+    style: {
+      justifyContent: 'center',
+      padding: '25px',
+    },
+  },
+  headRow: {
+    style: {
+      borderBottomWidth: '1px',
+      borderBottomColor: '#000',
+    },
+  },
+  rows: {
+    highlightOnHoverStyle: {
+      transform: 'translateY(-1px)',
+      backgroundColor: 'white',
+      boxShadow: '5px 5px 14px #f0f0f0, -5px -5px 14px #fafafa',
+      transition: 'all 0.2s ease-in-out',
+    },
+  },
+};
+
 const Table: FC<TableContentProps> = (props) => {
+  const router = useRouter();
+
   return (
-    <>
-      <table className={s.tableContainer}>
-        <thead>
-          {props.headers.map((header) => (
-            <th key={header}>{header}</th>
-          ))}
-        </thead>
-        <tbody>{props.children}</tbody>
-      </table>
-    </>
+    <DataTable
+      columns={props.columns}
+      data={props.data}
+      pagination
+      highlightOnHover
+      pointerOnHover
+      onRowClicked={(row) => {
+        if (row.curriculum) router.push(`/${props.route}/${row.curriculum.id}`);
+        else router.push(`/${props.route}/${row.id}`);
+      }}
+      customStyles={CUSTOM_STYLES}
+    />
   );
 };
 
