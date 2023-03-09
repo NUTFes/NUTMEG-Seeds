4a07a0cca5ba0990dd93a64264a98a87c44f57cc
YushiroDodo63 <yushiro0603@gmail.com>
2023-03-09 12:20:27 +0900

    [fix] chapter to curriculum

--- view/next-project/src/components/common/CurriculumDeleteModal.tsx
+++ view/next-project/src/components/common/CurriculumDeleteModal.tsx
@@ -4,24 +4,24 @@ import { del } from '@utils/api_methods';
 import Button from '@components/common/TestButton';
 import { useUI } from '@components/ui/context';
 
-export default function ChapterDeleteModal() {
+export default function CurriculumDeleteModal() {
   const router = useRouter();
   const query = router.query;
   const { closeModal } = useUI();
 
-  const DeleteChapter = async (query: any) => {
-    const deleteChapterUrl = process.env.CSR_API_URI + '/chapters/' + query.id;
-    await del(deleteChapterUrl);
+  const DeleteCurriculum = async (query: any) => {
+    const deleteCurriculumUrl = process.env.CSR_API_URI + '/curriculums/' + query.id;
+    await del(deleteCurriculumUrl);
     router.back();
   };
 
   return (
     <>
-      <h2>Delete Chapter</h2>
+      <h2>Delete Curriculum</h2>
       <h3>Are you sure?</h3>
       <Button
         onClick={() => {
-          DeleteChapter(query);
+          DeleteCurriculum(query);
           closeModal();
         }}
       >
