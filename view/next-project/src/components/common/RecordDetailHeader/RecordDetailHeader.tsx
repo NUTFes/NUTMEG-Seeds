9f7e0e3d2de55db2bf22dba704809f8a6dfef24a
YushiroDodo63 <yushiro0603@gmail.com>
2023-03-17 00:27:25 +0900

    [fix] curriculum&chapter, thacher&userを2行に変更

--- view/next-project/src/components/common/RecordDetailHeader/RecordDetailHeader.tsx
+++ view/next-project/src/components/common/RecordDetailHeader/RecordDetailHeader.tsx
@@ -24,10 +24,14 @@ const RecordDetailHeader = (props: Props) => {
     <div className={s.HeaderContainer}>
       <div className={s.TitleContainer}>{props.recordTitle}</div>
       <div className={s.TextContainer}>
-        <div className={s.DescriptionContainer}>Curriclum:{props.curriculumTitle}</div>
-        <div className={s.DescriptionContainer}>Chapter:{props.chapterTitle}</div>
-        <div className={s.DescriptionContainer}>Teacher:{props.teacher}</div>
-        <div className={s.DescriptionContainer}>User:{props.user}</div>
+        <div>
+          <div className={s.DescriptionContainer}>Curriclum:{props.curriculumTitle}</div>
+          <div className={s.DescriptionContainer}>Chapter:{props.chapterTitle}</div>
+        </div>
+        <div>
+          <div className={s.DescriptionContainer}>Teacher:{props.teacher}</div>
+          <div className={s.DescriptionContainer}>User:{props.user}</div>
+        </div>
       </div>
       <div className={s.TextContainer}>
         <div className={s.DescriptionContainer}>Create Date:{props.createDate}</div>
