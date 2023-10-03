7e7eb2fee2c4e371076712e4a3e25670da6acd71
imaimai17468 <tosiki_801_life@outlook.jp>
2023-10-03 18:59:12 +0900

    [feat] minio関連のapi routeを整備

---
+++ view/next-project/src/pages/api/images/index.ts
@@ -0,0 +1,53 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { formidable } from "formidable";
+
+const Minio = require("minio");
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const minioClient = new Minio.Client({
+  endPoint: process.env.NEXT_PUBLIC_ENDPOINT,
+  port: Number(process.env.NEXT_PUBLIC_PORT),
+  accessKey: process.env.NEXT_PUBLIC_ACCESS_KEY,
+  secretKey: process.env.NEXT_PUBLIC_SECRET_KEY,
+  useSSL: false,
+});
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const form = formidable();
+    const fs = require("fs");
+
+    form.parse(req, async (err, fields, files: any) => {
+      if (err) {
+        throw new Error("Error parsing form: " + err);
+      }
+
+      const bucketName = fields.bucketName;
+      const fileName = fields.fileName;
+      const metaData = {
+        "Content-Type": "image/png",
+      };
+
+      try {
+        const response = await minioClient.putObject(
+          bucketName,
+          fileName,
+          fs.createReadStream(files.file.filepath),
+          metaData
+        );
+
+        res.status(200).json({ response });
+      } catch (err) {
+        throw new Error("Error uploading file (" + err + ")");
+      }
+    });
+  }
+}
